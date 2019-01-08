3c6bab5bb12653a8a37840b7b4b6c8d6f52306e4
Connor Curnin <pixelq6@gmail.com>
2019-01-08 15:18:54 -0500

    cleaning

---
+++ DataMatch/DataMatch/MenuViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DataMatch
+//
+//  Created by Frank on 12/31/18.
+//  Copyright © 2018 Frank Kulaszewicz. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBAction func Begin_Survey(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var DataMatch_Title: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DataMatch/SurveyViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SurveyViewController.swift
+//  DataMatch
+//
+//  Created by Frank on 12/31/18.
+//  Copyright © 2018 Frank Kulaszewicz. All rights reserved.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    @IBOutlet weak var Question: UILabel!
+    
+    @IBAction func Answer_A(_ sender: Any) {
+    }
+    @IBAction func Answer_B(_ sender: Any) {
+    }
+    @IBAction func Answer_C(_ sender: Any) {
+    }
+    @IBAction func Answer_D(_ sender: Any) {
+    }
+    @IBAction func Answer_E(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
