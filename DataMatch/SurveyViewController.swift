b5ac3c4d3ec653015ce848c9739159a8d66baaca
jhcarrington <43937870+jhcarrington@users.noreply.github.com>
2019-01-09 12:49:59 -0800

    Update SurveyViewController.swift

--- DataMatch/SurveyViewController.swift
+++ DataMatch/SurveyViewController.swift
@@ -2,7 +2,7 @@
 //  SurveyViewController.swift
 //  DataMatch
 //
-//  Created by Frank on 12/31/18.
+//  Created by Jason Carrington on 1/8/18.
 //  Copyright © 2018 Frank Kulaszewicz. All rights reserved.
 //
 
@@ -10,38 +10,31 @@ import UIKit
 
 class SurveyViewController: UIViewController {
 
-    var answers: [Int] = []
-    
     @IBOutlet weak var Question: UILabel!
     
     @IBAction func Answer_A(_ sender: Any) {
-        answers.append(0);
-        // Code to transition to next question and answers
     }
     @IBAction func Answer_B(_ sender: Any) {
-        answers.append(1);
-        // ""
     }
     @IBAction func Answer_C(_ sender: Any) {
-        answers.append(2);
-        // ""
     }
     @IBAction func Answer_D(_ sender: Any) {
-        answers.append(3);
-        // ""
     }
     @IBAction func Answer_E(_ sender: Any) {
-        answers.append(4);
-        // ""
     }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let parsed: ToParse? = parse()
+        
+        if let parsingJson = parsed{
+            print(parsingJson.survey[1].question) //use this to find the values of interest, gets the values of the fields in the specified struct
+        }
+        
     }
+    
     
     
     /*
